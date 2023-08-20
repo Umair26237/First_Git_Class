15a383488394da121f1cfdf3b0f9156e8d1e2acc
Umair26237 <umairshaheen45@gmail.com>
2023-08-20 14:21:29 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,72 @@
+// console.log("hello class");
+
+
+// write a stype script prog that cal of sum of even # 1 to 1000;
+// var sum:number=0;
+// var list:number[]=[];
+// for(let index=0;index<=100000; index++){
+// if (index % 2 ==0) {
+//     sum = sum + index;
+//     list.push(index)
+// } 
+// }
+// console.log("sum", sum, list);
+
+
+//let i:number=1;
+// do {
+//     console.log("naveed");
+    
+//     i++
+// }while (i < 10)
+
+
+// let i:number=1;
+// do {
+//     console.log("naveed");
+
+
+
+//     i++
+// }while (i < 10)
+
+
+
+
+//var index:number = 1;
+//for (let index=10;index>=5;index--){
+// console.log("naveed",index);
+
+// }
+
+//for (let index=1;index <10;index++){ // in for we will do a code in single line
+//console.log(index);
+
+//}
+// while(index <10){ // in while we will do a code in different lines or 3 lines
+// console.log("index");
+// index++
+ // }
+ // console.log("console");
+ 
+ 
+ 
+// index=index+1
+// var result:number= index++
+// var result:number= ++index
+// console.log("index",index);
+//console.log('result',result);
+
+// index++
+// index--
+// index=index -1 
+
+// ++index;
+// --index;
+
+//console.log("index",index);
+
+
+
+
+ 
